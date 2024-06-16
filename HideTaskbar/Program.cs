15bf437860cad0dcfcd12021f05c1edd02626799
WwwwTgd <956095819@qq.com>
2024-06-16 14:47:40 +0800

    运行前检测是否存在实例，确保不会创建多个实例。

--- HideTaskbar/Program.cs
+++ HideTaskbar/Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace HideTaskbar
 {
     internal static class Program
@@ -8,6 +10,17 @@ namespace HideTaskbar
         [STAThread]
         static void Main()
         {
+            // 获取当前进程的名称
+            string processName = Process.GetCurrentProcess().ProcessName;
+            Process[] runningProcesses = Process.GetProcessesByName(processName);
+
+            // 如果已经存在相同的进程，则显示信息并退出
+            if (runningProcesses.Length > 1)
+            {
+                MessageBox.Show("应用程序已经在运行中。", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
