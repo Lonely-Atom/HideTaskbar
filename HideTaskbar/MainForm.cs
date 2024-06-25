8c8bc47c284f038cb1f1971fc7cfe9a59bd4572b
LonelyAtom <46003532+Lonely-Atom@users.noreply.github.com>
2024-06-26 00:45:20 +0800

    修改 Readme 内容
    修改 Readme 内容

--- HideTaskbar/MainForm.cs
+++ HideTaskbar/MainForm.cs
@@ -251,17 +251,18 @@ namespace HideTaskbar
             sb_msg.AppendLine("【软件名】：隐藏任务栏小工具\n");
 
             sb_msg.AppendLine("【重要提醒】：");
-            sb_msg.AppendLine("    1.本软件为免费提供，作者为「LonelyAtom」。任何索要付费购买此软件的行为均为欺诈。请勿向任何第三方支付费用，以免受到欺骗。");
-            sb_msg.AppendLine("    2.本软件受到版权保护，并且仅限于合法获得许可的用户使用。未经授权的复制、分发或盗版行为将依法追究其法律责任。\n");
+            sb_msg.AppendLine("    1. 本软件为免费提供，作者为「LonelyAtom」。任何索要付费购买此软件的行为均为欺诈。请勿向任何第三方支付费用，以免受到欺骗。");
+            sb_msg.AppendLine("    2. 本软件受到版权保护，并且仅限于合法获得许可的用户使用。未经授权的复制、分发或盗版行为将依法追究其法律责任。\n");
 
             sb_msg.AppendLine("【重要说明】：");
-            sb_msg.AppendLine("    1.此软件无窗口，运行后将自动收到任务栏托盘中。");
-            sb_msg.AppendLine("    2.隐藏任务栏后托盘也会被隐藏，所以请熟记以下快捷键，防止隐藏后无法恢复。");
-            sb_msg.AppendLine("    3.双击任务栏图标可以【隐藏/显示任务栏】。\n");
+            sb_msg.AppendLine("    1. 此软件无窗口，运行后将自动收到任务栏托盘中。");
+            sb_msg.AppendLine("    2. 隐藏任务栏后托盘也会被隐藏，所以请熟记以下快捷键，防止隐藏后无法恢复。");
+            sb_msg.AppendLine("    3. 双击任务栏图标可以【隐藏/显示任务栏】。");
+            sb_msg.AppendLine("    4. 在任务栏隐藏时，可使用快捷键将系统托盘呼出（目前已知 Win11 无效）。\n");
 
             sb_msg.AppendLine("【快捷键】（可在 Settings.ini 文件中修改）：");
             sb_msg.AppendLine("    1.隐藏/显示任务栏：【Ctrl + ~】");
-            sb_msg.AppendLine("    2.隐藏/显示系统托盘：【Alt + 1】（目前已知 Win11 无效）\n");
+            sb_msg.AppendLine("    2.隐藏/显示系统托盘：【Alt + 1】\n");
 
             sb_msg.AppendLine("【作者】：LonelyAtom\n");
             sb_msg.AppendLine("【贡献者】：Wwwwtgd");
